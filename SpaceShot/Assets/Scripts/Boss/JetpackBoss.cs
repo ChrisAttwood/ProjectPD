9f5277d621cd2ea1bb1f82fc2e631dd4d43a1182
SebCharman <cheesemonkstylee@hotmail.com>
2019-07-29 22:43:09 +0100

    Moving Boss
    Boss now flies about, scripts are pretty poorly written (i.e. hard coded to the world location where I know the boss currently spawns)

---
+++ SpaceShot/Assets/Scripts/Boss/JetpackBoss.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetpackBoss : MonoBehaviour
+{
+    public Boss Boss;
+    public List<MovementPath> availablePaths;
+    public Rigidbody2D RigidBody2D;
+    public float Speed;
+    private MovementPath movementPath;
+    private int currentPathNode = 0;
+    private Vector2 endPoint;
+
+    private void Update()
+    {
+        if (movementPath == null)
+        {
+            ChooseNewPath();
+        }
+        else
+        {
+            if (PathEndReached())
+            {
+                ChooseNewPath();
+            } else
+            {
+                if (NextNodeReached())
+                {
+                    currentPathNode++;
+                }
+                FollowPath();
+            }
+        }
+    }
+
+    private void ChooseNewPath()
+    {
+        Debug.Log("Choose New Path");
+        int roll = Random.Range(0, availablePaths.Count);
+        movementPath = availablePaths[roll];
+        endPoint = movementPath.path[movementPath.path.Count - 1];
+        currentPathNode = 0;
+    }
+
+    private bool PathEndReached()
+    {
+        bool end = ((Vector2) transform.position == endPoint);
+        Debug.Log("Path end check = " + end);
+        return end;
+    }
+
+    private bool NextNodeReached()
+    {
+        bool reached = ((Vector2)transform.position == movementPath.path[currentPathNode]);
+        Debug.Log("Node reached check = " + reached);
+        return reached;
+    }
+
+    private void FollowPath()
+    {
+        Debug.Log("Moving");
+        transform.position = Vector2.MoveTowards(transform.position, movementPath.path[currentPathNode], (Speed * Time.deltaTime));
+
+    }
+}
---
+++ SpaceShot/Assets/Scripts/Boss/MovementPath.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MovementPath
+{
+    public List<Vector2> path;
+}
